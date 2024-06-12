086dc2ca7e7407f360eea3352554fc6e07de78e4
T. sanjay karthik <108615710+tsanjaykarthik@users.noreply.github.com>
2024-06-12 11:27:28 +0530

    responsive layout (format)

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 const App = () => {
   return (
     <Grid
-      templateAreas={ {base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`}}
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
       <GridItem pl="2" bg="pink.300" area={"nav"}>
         Nav
@@ -14,9 +13,9 @@ const App = () => {
         Main
       </GridItem>
       <Show above="lg">
-      <GridItem pl="2" bg="blue.300" area={"aside"}>
-        aside
-      </GridItem>
+        <GridItem pl="2" bg="blue.300" area={"aside"}>
+          aside
+        </GridItem>
       </Show>
     </Grid>
   );
