45f1c4cd9b91648b17a8fe39e1ccf180291b9561
T. sanjay karthik <108615710+tsanjaykarthik@users.noreply.github.com>
2024-06-12 11:26:49 +0530

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import React from "react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-     <Button colorScheme='blue'>Button</Button>
-  )
-}
+    <Grid
+      templateAreas={ {base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`}}
+    >
+      <GridItem pl="2" bg="pink.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+      <Show above="lg">
+      <GridItem pl="2" bg="blue.300" area={"aside"}>
+        aside
+      </GridItem>
+      </Show>
+    </Grid>
+  );
+};
 
-export default App
+export default App;
